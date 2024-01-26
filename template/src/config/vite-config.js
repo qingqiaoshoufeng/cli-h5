15bf61f9ab3186d8460395d36f965f82a091cb96
hexiaofei <hexiaofei@hxdi.com>
2024-01-26 17:57:05 +0800

    feat: 增加代理配置

--- template/src/config/vite-config.js
+++ template/src/config/vite-config.js
@@ -1,8 +1,19 @@
 export default {
-  server: ({ env }) => {
-    return {}
-  },
   vite: ({ env }) => {
     return {}
+  },
+  server: ({ env }) => {
+    return {
+      host: true,
+      open: true,
+      port: 3000,
+      proxy: {
+        '/acws': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    }
   },
 }
