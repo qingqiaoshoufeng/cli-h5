672f7391d739dbcb89982fd44a728ab2f951af30
yuzhiqiang <yuzhiqiang2@hxdi.com>
2024-02-28 09:37:18 +0800

    feat: 添加一个测试页面

---
+++ template/src/pages/test/index.vue
@@ -0,0 +1,27 @@
+<route lang="json">
+  {
+    "meta": {
+      "title": "测试页面",
+      "icon": "ReadFilled",
+      "requiresAuth": true,
+      "sort": 8,
+      "permissions": ["admin", "business-manage"],
+      "hideInMenu": false,
+      "keepAlive": "BusinessManage"
+    }
+  }
+  </route>
+
+<script>
+export default {
+  name: 'BusinessManage',
+}
+</script>
+
+<template>
+  <div>
+    测试页面
+  </div>
+</template>
+
+<style lang="less" scoped></style>
